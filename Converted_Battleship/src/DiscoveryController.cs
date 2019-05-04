2f1685bc48e3f2be4efec1c198fc6f2e1c76b82c
Obadiah <101219548@students.swinburne.edu.my>
2019-05-04 19:47:26 +0800

    (Obadiah)Add cheat to see AI ship

--- Converted_Battleship/src/DiscoveryController.cs
+++ Converted_Battleship/src/DiscoveryController.cs
@@ -5,12 +5,13 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using SwinGameSDK;
 
+
 /// <summary>
 /// The battle phase is handled by the DiscoveryController.
 /// </summary>
 static class DiscoveryController
 {
-
+	
 	/// <summary>
 	/// Handles input during the discovery phase of the game.
 	/// </summary>
@@ -23,6 +24,7 @@ static class DiscoveryController
 		if (SwinGame.KeyTyped(KeyCode.vk_ESCAPE)) {
 			GameController.AddNewState(GameState.ViewingGameMenu);
 		}
+			
 		/// <summary>
 		/// When the mouse's left button clicked the ship will attack the location that mouce pointed. 
 		/// </summary>
@@ -70,35 +72,40 @@ static class DiscoveryController
 		const int HITS_TOP = 206;
 		const int SPLASH_TOP = 256;
 
-
-		if ((SwinGame.KeyDown(KeyCode.vk_LSHIFT) | SwinGame.KeyDown(KeyCode.vk_RSHIFT)) & SwinGame.KeyDown(KeyCode.vk_c)) {
-			UtilityFunctions.DrawField(GameController.HumanPlayer.EnemyGrid, GameController.ComputerPlayer, true);
-		} else {
-			UtilityFunctions.DrawField(GameController.HumanPlayer.EnemyGrid, GameController.ComputerPlayer, false);
-		}
-
-		UtilityFunctions.DrawSmallField(GameController.HumanPlayer.PlayerGrid, GameController.HumanPlayer);
+				UtilityFunctions.DrawSmallField(GameController.HumanPlayer.PlayerGrid, GameController.HumanPlayer);
 		UtilityFunctions.DrawMessage();
 
 		//Drawing the available shots for player in medium and hard level
 		if (GameController.ComputerPlayer.ToString ().Equals ("AIMediumPlayer"))
 		{
+			
 			SwinGame.DrawText ("Available Shot(s) : ", Color.White, GameResources.GameFont ("Menu"), 172, 100);
 			SwinGame.DrawText (GameController.HumanPlayer.Available.ToString (), Color.White, GameResources.GameFont ("Menu"), 310, 100);
+			SwinGame.DrawText ("Time Left : ", Color.White, GameResources.GameFont ("Menu"), 350, 100);
+
 		}
 		else if (GameController.ComputerPlayer.ToString ().Equals ("AIHardPlayer"))
 		{
 			SwinGame.DrawText ("Available Shot(s) : ", Color.White, GameResources.GameFont ("Menu"), 172, 100);
 			SwinGame.DrawText (GameController.HumanPlayer.Available.ToString (), Color.White, GameResources.GameFont ("Menu"), 310, 100);
-		}
+			SwinGame.DrawText ("Time Left : "  , Color.White, GameResources.GameFont ("Menu"), 350, 100);
 
+		}
+			
 		SwinGame.DrawBitmap(GameResources.GameImage("PauseButton"), 693, 72);
 		SwinGame.DrawText(GameController.HumanPlayer.Shots.ToString(), Color.White, GameResources.GameFont("Menu"), SCORES_LEFT, SHOTS_TOP);
 		SwinGame.DrawText(GameController.HumanPlayer.Hits.ToString(), Color.White, GameResources.GameFont("Menu"), SCORES_LEFT, HITS_TOP);
 		SwinGame.DrawText(GameController.HumanPlayer.Missed.ToString(), Color.White, GameResources.GameFont("Menu"), SCORES_LEFT, SPLASH_TOP);
 		SwinGame.DrawText (GameController.count.ToString (), Color.White, GameResources.GameFont ("Menu"), SCORES_LEFT, 306);
 		//let computer ship visiable
-		UtilityFunctions.DrawField (GameController.ComputerPlayer.PlayerGrid, GameController.ComputerPlayer, true);
+		UtilityFunctions.DrawField (GameController.ComputerPlayer.PlayerGrid, GameController.ComputerPlayer, false);
+
+		//Cheat code
+		if (SwinGame.KeyDown(KeyCode.vk_c)) {
+			UtilityFunctions.DrawField (GameController.ComputerPlayer.PlayerGrid, GameController.ComputerPlayer, true);
+		} else {
+			UtilityFunctions.DrawField (GameController.ComputerPlayer.PlayerGrid, GameController.ComputerPlayer, false);
+		}
 	}
 
 }
