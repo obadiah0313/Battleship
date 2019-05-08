9ceb0209f2491d1df659cb6e1b908aeb3fd4da87
Obadiah <101219548@students.swinburne.edu.my>
2019-05-08 22:33:26 +0800

    (Obadiah) Draw timer text on the gameplay screen

--- Converted_Battleship/src/DiscoveryController.cs
+++ Converted_Battleship/src/DiscoveryController.cs
@@ -81,14 +81,14 @@ static class DiscoveryController
 			
 			SwinGame.DrawText ("Available Shot(s) : ", Color.White, GameResources.GameFont ("Menu"), 172, 100);
 			SwinGame.DrawText (GameController.HumanPlayer.Available.ToString (), Color.White, GameResources.GameFont ("Menu"), 310, 100);
-			//SwinGame.DrawText ("Time Left : ", Color.White, GameResources.GameFont ("Menu"), 350, 100);
+			SwinGame.DrawText ("Time Remaining: " + GameController.TimeLeft(480000) + " second(s)", Color.White, GameResources.GameFont ("Menu"), 420, 100);
 
 		}
 		else if (GameController.ComputerPlayer.ToString ().Equals ("AIHardPlayer"))
 		{
 			SwinGame.DrawText ("Available Shot(s) : ", Color.White, GameResources.GameFont ("Menu"), 172, 100);
 			SwinGame.DrawText (GameController.HumanPlayer.Available.ToString (), Color.White, GameResources.GameFont ("Menu"), 310, 100);
-			//SwinGame.DrawText ("Time Left : "  , Color.White, GameResources.GameFont ("Menu"), 350, 100);
+			SwinGame.DrawText ("Time Remaining: " + GameController.TimeLeft(300000) + " second(s)", Color.White, GameResources.GameFont ("Menu"), 400, 100);
 
 		}
 			
@@ -97,9 +97,13 @@ static class DiscoveryController
 		SwinGame.DrawText(GameController.HumanPlayer.Hits.ToString(), Color.White, GameResources.GameFont("Menu"), SCORES_LEFT, HITS_TOP);
 		SwinGame.DrawText(GameController.HumanPlayer.Missed.ToString(), Color.White, GameResources.GameFont("Menu"), SCORES_LEFT, SPLASH_TOP);
 		SwinGame.DrawText (GameController.count.ToString (), Color.White, GameResources.GameFont ("Menu"), SCORES_LEFT, 306);
+
+
 		//let computer ship visiable
 		UtilityFunctions.DrawField (GameController.ComputerPlayer.PlayerGrid, GameController.ComputerPlayer, false);
 		SwinGame.DrawText ("Press \"C\" to show AI ships", Color.White, GameResources.GameFont ("Menu"), 172, 570);
+
+
 		//Cheat code
 		if (SwinGame.KeyDown(KeyCode.vk_c)) {
 			UtilityFunctions.DrawField (GameController.ComputerPlayer.PlayerGrid, GameController.ComputerPlayer, true);
