292ea042df449036cb7c203847d75e8d8bd16071
Obadiah <101219548@students.swinburne.edu.my>
2019-05-16 23:07:06 +0800

    (Obadiah) Add BattleshipUnitTesting class file

--- Converted_Battleship/BattleshipUnitTesting.cs
+++ Converted_Battleship/BattleshipUnitTesting.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using NUnit.Framework;
+using SwinGameSDK;
 namespace MyGame
 {
+	[TestFixture]
 	public class BattleshipUnitTesting
 	{
-		public BattleshipUnitTesting ()
+		[Test]
+		public void AddPlayerTest ()
 		{
+			BattleShipsGame testGame = new BattleShipsGame ();
+			Player p1 = new Player (testGame);
+			testGame.AddDeployedPlayer (p1);
+			Assert.AreEqual (p1, testGame.Player);
+		}
+
+		[Test]
+		public void IsDestroyedTest ()
+		{
+			Ship testShip = new Ship (ShipName.AircraftCarrier);
+			for (int i = 0; i < 5; i++) { testShip.Hit (); }
+			bool expected = true;
+			bool actual = testShip.IsDestroyed;
+			Assert.AreEqual (expected, actual);
 		}
 	}
 }
